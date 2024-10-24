95501e9bf70ed5e7d0ef9a1ea89e7590e4d56d69
acheNoah <lukas.achernig@edu.fh-joanneum.at>
2024-10-24 12:56:10 +0200

    Plululululululululu

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -10,6 +12,20 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+
+            string schnacksln;
+
+            for (int =i; i < schnacksln.lenght; i++){
+                System.out.println("wer hot do karotten eineton, seind de teipad");
+                System.out.println("wie oft wird flo heute schnacksln" +schnacksln.length);
+
+            }
+            int handynummer;
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Bitte gib deine Handynummer ein: ");
+            handynummer = scanner.nextInt();
+
         }
     }
 }
